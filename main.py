00395a3c2b164b97cd1a726922d3db1c67ec295c
Adarsh Gupta <113763427+apneduniya@users.noreply.github.com>
2024-08-05 06:27:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 
 
-def add(a: str, b: int) -> int:
-    """
-    This function adds two numbers.
-    @param a: The first number
-    @param b: The second number
-    @return: The sum of the two numbers
-    """
-
-    return a + b
+def add(a: int, b: int) -> int:
+    return a - b
 
 
-result = sub(1, 2)
+result = add(1, 2)
 print(result)
